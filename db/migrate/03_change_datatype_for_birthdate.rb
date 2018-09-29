32b00e2f57d465a7029c3143f0926a5d6e3a079d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-29 10:18:00 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
 
   def change
-    change_column :students :birthdate :datetime)
+    change_column :students :birthdate :datetime
   end
 
 end
